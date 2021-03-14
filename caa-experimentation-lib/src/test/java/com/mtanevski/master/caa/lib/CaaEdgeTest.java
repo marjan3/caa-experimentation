7fc7dfe5ba7a7a64539fde2beade70af89155c79
Marjan Tanevski <marjantanevski@outlook.com>
2021-03-14 14:47:13 +0100

    Remove space from SSH

--- caa-experimentation-lib/src/test/java/com/mtanevski/master/caa/lib/CaaEdgeTest.java
+++ caa-experimentation-lib/src/test/java/com/mtanevski/master/caa/lib/CaaEdgeTest.java
@@ -11,7 +11,6 @@ public class CaaEdgeTest {
         CaaEdge edge = new CaaEdge("a", "b", 10.0, -10.0);
 
         assertEquals("a", edge.getFrom());
-
         assertEquals("b", edge.getTo());
         assertEquals(10.0, edge.getWeight(), 0);
         assertEquals(-10.0, edge.getTraversalWeight(), 0);
